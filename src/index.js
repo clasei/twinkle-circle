62d63addc1bbeedde2cf7e93991424bf3eccd459
clasei <clasei@proton.me>
2023-10-22 20:22:17 +0200

    window.global position changed on index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-if (typeof window !== 'undefined' && typeof window.global === 'undefined') {
-  window.global = window;
-}
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+if (typeof window !== 'undefined' && typeof window.global === 'undefined') {
+  window.global = window;
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
