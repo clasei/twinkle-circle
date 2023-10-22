913072a96ec2ce4c054b22a0cfccab8b816ce5a3
clasei <clasei@proton.me>
2023-10-22 19:50:36 +0200

    style updated, circles to square

--- src/components/Circle/Circle.js
+++ src/components/Circle/Circle.js
@@ -4,7 +4,6 @@ import './Circle.css';
 const Circle = ({ color, onClick, active }) => {
     const circleStyle = {
         backgroundColor: color,
-        // ...otros estilos según sea necesario
     };
 
     return <div className="circle" style={circleStyle} onClick={onClick}></div>;
--- src/components/CircleContainer/CircleContainer.js
+++ src/components/CircleContainer/CircleContainer.js
@@ -5,16 +5,13 @@ import './CircleContainer.css';
 const CircleContainer = () => {
     return (
         <div className="container">
-            <div className="circle-row">
-                <Circle color="pink" onClick={() => console.log('Pink circle clicked!')} />
-                <Circle color="green" onClick={() => console.log('Green circle clicked!')} />
-            </div>
-            <div className="circle-row">
-                <Circle color="blue" onClick={() => console.log('Blue circle clicked!')} />
-                <Circle color="yellow" onClick={() => console.log('Yellow circle clicked!')} />
-            </div>
+            <Circle color="pink" onClick={() => console.log('Pink circle clicked!')} />
+            <Circle color="green" onClick={() => console.log('Green circle clicked!')} />
+            <Circle color="blue" onClick={() => console.log('Blue circle clicked!')} />
+            <Circle color="yellow" onClick={() => console.log('Yellow circle clicked!')} />
         </div>
     );
 }
 
 export default CircleContainer;
+
