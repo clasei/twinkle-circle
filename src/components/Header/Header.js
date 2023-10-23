151e9d2b6563c2de988575edbcafe476e1ea088c
clara <clasei@proton.me>
2023-10-23 15:54:07 +0200

    title function implemented, weird result

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 
 const Header = () => {
+    const [displayedTitle, setDisplayedTitle] = useState('');
+
+    useEffect(() => {
+        const titleText = 'twinkleCircle';
+        let letters = titleText.split('');
+
+        // array with the letters, random order
+        let indexes = Array.from({length: letters.length}, (_, i) => i);
+        indexes.sort(() => Math.random() - 0.5);
+
+        let newTitle = '';
+        indexes.forEach((index, i) => {
+            setTimeout(() => {
+                newTitle += letters[index];
+                setDisplayedTitle(prev => prev + letters[index]);
+            }, i * 100); // interval ms
+        });
+    }, []);
+
     return (
         <div className="header">
-            <h1 className="title">twinkleCircle</h1>
+            <h1 className="title">{displayedTitle}</h1>
         </div>
     );
 }
