ab06c739efa5b91a06c25288c3255cb05cb5884f
clara <clasei@proton.me>
2023-10-23 11:10:53 +0200

    particles added, style updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import particlesJS from 'particles.js';
 import './App.css';
 import CircleContainer from './components/CircleContainer/CircleContainer';
 import ControlPanel from './components/ControlPanel/ControlPanel';
@@ -8,12 +9,19 @@ function App() {
       console.log("Game started!");
   };
 
+  useEffect(() => {
+        particlesJS.load('particles-js', './assets/particles.json', function() {
+            console.log('particles.js loaded - callback');
+        });
+    }, []);
+
   return (
-      <div className="App">
-          <CircleContainer />
-          <ControlPanel onStart={startGame} />
-      </div>
-  );
+    <div className="App">
+        <div id="particles-js"></div>
+        <CircleContainer />
+        <ControlPanel onStart={startGame} />
+    </div>
+ );
 }
 
 export default App;
