75b96ffa8a0bb0ccee4a4a6f10bbeca87b1ea6e3
clara <clasei@proton.me>
2023-10-23 17:12:37 +0200

    styles improved

--- src/assets/particles.js
+++ src/assets/particles.js
@@ -1,32 +1,32 @@
 const particlesConfig = {
     "particles": {
       "number": {
-        "value": 30,
+        "value": 20,
         "density": {
           "enable": true,
-          "value_area": 500
+          "value_area": 300
         }
       },
       "color": {
-        "value": "#AAAAAA"
+        "value": "#8E8E8E"
       },
       "shape": {
         "type": "circle",
         "opacity": 0.3,
-        "color": "#AAAAAA"
+        "color": "#8E8E8E"
       },
       "opacity": {
         "value": 0.3,
         "random": true
       },
       "size": {
-        "value": 2,
+        "value": 0.7,
         "random": true
       },
       "line_linked": {
         "enable": true,
         "opacity": 0.3,
-        "color": "#AAAAAA"
+        "color": "#8E8E8E"
       },
       "move": {
         "enable": true,
