dab6b3cf9a8fe47e35e4c6d4e8694733f1d2b7e0
clara <clasei@proton.me>
2023-10-24 21:55:28 +0200

    footer style improved here

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div id="footer">
         <footer>
-            <span id="currentYear">{ currentYear }</span>
+            <span id="currentYear">{ currentYear } </span>
             // built by <a href="https://github.com/clasei" target="_blank" rel="noopener noreferrer">clara </a>
             <span id="rocketIcon">🚀</span>
             pragmatic front-end developer <a href="https://bio.link/claraseijo" target="_blank" rel="noopener noreferrer">→ clara seijo</a>
