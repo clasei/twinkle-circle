907c0e070c2692e2aad51a6d561fbb92ef610c03
clara <clasei@proton.me>
2023-10-24 15:56:09 +0200

    speed slider style implemented

--- src/components/ControlPanel/ControlPanel.js
+++ src/components/ControlPanel/ControlPanel.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './ControlPanel.css';
 
-const ControlPanel = ({ onStart }) => {
+const ControlPanel = ({ onStart, gameSpeed, onSpeedChange }) => {
   return (
     <div className="control-panel">
       <p>watch & repeat — that's it</p>
       <button onClick={onStart}>play</button>
+      <p>- adapt the speed -</p>
+      <input
+          type="range"
+          min="200"
+          max="1000"
+          value={gameSpeed}
+          onChange={onSpeedChange}
+          id="speedSlider"
+      />
+      <span>{gameSpeed}ms</span>
     </div>
   );
 }
