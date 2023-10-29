c86745fadf5b4da3a74a4b5ed551bdd0f617bb5f
clara <clasei@proton.me>
2023-10-29 07:38:22 +0100

    App.js refactorized

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   const [gameSpeed, setGameSpeed] = useState(500);
   const colors = ["pink", "blue", "green", "yellow"];
   const [activeCircle, setActiveCircle] = useState("");
+  const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
   const headerRef = useRef(null);
 
@@ -37,13 +38,13 @@ function App() {
 
   const playSequence = async (sequence) => {
     setActiveCircle("");
-    await new Promise(resolve => setTimeout(resolve, 850));
+    await sleep(850);
 
     for (let color of sequence) {
       setActiveCircle(color);
-      await new Promise(resolve => setTimeout(resolve, gameSpeed));
+      await sleep(gameSpeed);
       setActiveCircle("");
-      await new Promise(resolve => setTimeout(resolve, 250));
+      await sleep(250);
     }
   };
 
